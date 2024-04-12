1864d57738fd2cf4814b0d808fb42711c29fcf71
Adrian Sieber <mail@adriansieber.com>
2024-04-12 21:30:13 +0000

    Add Anthropic's Opus models, support running several models at once
    List meta information for each execution (e.g. runtime)

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,74 @@
-use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::str;
+use std::{collections::HashMap, time::Instant};
 
+use color_print::{cformat, cprintln};
 use config::Config;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 use textwrap::termwidth;
 use xdg::BaseDirectories;
 
+pub const OPENAI_GPT_TURBO: &str = "gpt-4-turbo";
+pub const OPENAI_GPT: &str = "gpt-4";
+pub const CLAUDE_OPUS: &str = "claude-3-opus-20240307";
+pub const CLAUDE_SONNET: &str = "claude-3-sonnet-20240307";
+pub const CLAUDE_HAIKU: &str = "claude-3-haiku-20240307";
+pub const GROQ_MIXTRAL: &str = "mixtral-8x7b-32768";
+
 #[derive(Serialize, Debug, PartialEq, Default, Clone, Copy)]
 pub enum Provider {
   #[default]
+  Anthropic,
   Groq,
   OpenAI,
   Local,
 }
 
-#[derive(Serialize, Debug, Default, Clone)]
+impl std::fmt::Display for Provider {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Provider::Anthropic => write!(f, "Anthropic"),
+      Provider::Groq => write!(f, "Groq"),
+      Provider::OpenAI => write!(f, "OpenAI"),
+      Provider::Local => write!(f, "Local"),
+    }
+  }
+}
+
+#[derive(Serialize, Debug, PartialEq, Clone)]
+pub enum Model {
+  Model(Provider, String),
+}
+
+impl Default for Model {
+  fn default() -> Model {
+    Model::Model(Provider::Anthropic, CLAUDE_HAIKU.to_string())
+  }
+}
+
+#[derive(Serialize, Debug, Clone)]
 struct AiRequest {
   provider: Provider,
   url: String,
   model: String,
   prompt: String,
+  max_tokens: u32,
   api_key: String,
+}
+
+impl Default for AiRequest {
+  fn default() -> AiRequest {
+    AiRequest {
+      provider: Default::default(),
+      url: Default::default(),
+      model: Default::default(),
+      prompt: Default::default(),
+      max_tokens: 4096,
+      api_key: Default::default(),
+    }
+  }
 }
 
 #[derive(Deserialize, Debug)]
@@ -45,18 +90,37 @@ struct AiResponse {
   choices: Vec<AiChoice>,
 }
 
+/// For Anthropic's API
+/// (https://docs.anthropic.com/claude/reference/messages_post)
+#[derive(Deserialize, Debug)]
+struct AnthropicAiContent {
+  text: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AnthropicAiResponse {
+  content: Vec<AnthropicAiContent>,
+}
+
 fn default_req_for_provider(provider: &Provider) -> AiRequest {
   match provider {
     Provider::Groq => AiRequest {
       provider: Provider::Groq,
       url: "https://api.groq.com/openai/v1/chat/completions".to_string(),
-      model: "mixtral-8x7b-32768".to_string(),
+      model: GROQ_MIXTRAL.to_string(),
       ..Default::default()
     },
     Provider::OpenAI => AiRequest {
       provider: Provider::OpenAI,
       url: "https://api.openai.com/v1/chat/completions".to_string(),
-      model: "gpt-4-turbo-preview".to_string(),
+      model: OPENAI_GPT_TURBO.to_string(),
+      ..Default::default()
+    },
+    Provider::Anthropic => AiRequest {
+      provider: Provider::Anthropic,
+      url: "https://api.anthropic.com/v1/messages".to_string(),
+      model: CLAUDE_HAIKU.to_string(),
+      max_tokens: 4096,
       ..Default::default()
     },
     Provider::Local => AiRequest {
@@ -71,9 +135,12 @@ fn get_key_setup_msg(secrets_path_str: &str) -> String {
   format!(
     "An API key must be provided. Use one of the following options:\n\
         \n\
-        1. Set `groq_api_key` or `openai_api_key` in {secrets_path_str}\n\
-        2. Set the env variable CAI_GROQ_API_KEY or GROQ_API_KEY\n\
-        3. Set the env variable CAI_OPENAI_API_KEY or OPENAI_API_KEY\n\
+        1. Set one or more API keys in {secrets_path_str}\n\
+           (`anthropic_api_key`, `groq_api_key`, `openai_api_key`)\n\
+        2. Set one or more cai specific env variables\n\
+            (CAI_ANTHROPIC_API_KEY, CAI_GROQ_API_KEY, CAI_OPENAI_API_KEY)\n\
+        3. Set one or more generic env variables\n\
+            (ANTHROPIC_API_KEY, GROQ_API_KEY, OPENAI_API_KEY)\n\
         ",
   )
 }
@@ -81,14 +148,16 @@ fn get_key_setup_msg(secrets_path_str: &str) -> String {
 fn get_api_request(
   full_config: &HashMap<String, String>,
   secrets_path_str: &str,
-  provider: &Provider,
+  model: &Model,
 ) -> Result<AiRequest, String> {
   let dummy_key = "DUMMY_KEY".to_string();
+  let Model::Model(provider, _) = model;
 
   {
     match provider {
       Provider::Groq => full_config.get("groq_api_key"),
       Provider::OpenAI => full_config.get("openai_api_key"),
+      Provider::Anthropic => full_config.get("anthropic_api_key"),
       Provider::Local => Some(&dummy_key),
     }
   }
@@ -107,10 +176,21 @@ fn get_api_request(
   })
 }
 
-async fn exec_tool(
-  optional_provider: &Option<Provider>,
+fn get_used_model(model: &Model) -> String {
+  let Model::Model(provider, model_id) = model;
+
+  if model_id.is_empty() {
+    cformat!("<bold>🧠 Model: {}</bold>\n", provider)
+  } else {
+    cformat!("<bold>🧠 Model: {}'s {}</bold>\n", provider, model_id)
+  }
+}
+
+pub async fn exec_tool(
+  optional_model: &Option<Model>,
   user_input: &str,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+  let start = Instant::now();
   let xdg_dirs = BaseDirectories::with_prefix("cai").unwrap();
   let secrets_path = xdg_dirs
     .place_config_file("secrets.yaml")
@@ -121,6 +201,10 @@ async fn exec_tool(
   let secrets_path_str = secrets_path.to_str().unwrap();
 
   let config = Config::builder()
+    .set_default(
+      "anthropic_api_key",
+      env::var("ANTHROPIC_API_KEY").unwrap_or_default(),
+    )?
     .set_default(
       "openai_api_key",
       env::var("OPENAI_API_KEY").unwrap_or_default(),
@@ -138,19 +222,31 @@ async fn exec_tool(
     .try_deserialize::<HashMap<String, String>>()
     .unwrap();
 
-  let http_req = match optional_provider {
-    Some(Provider::OpenAI) => {
-      get_api_request(&full_config, secrets_path_str, &Provider::OpenAI)?
+  let used_model: String;
+  let http_req = match optional_model {
+    Some(model) => {
+      used_model = get_used_model(&model);
+      get_api_request(&full_config, secrets_path_str, model)?
     }
-    Some(Provider::Local) => {
-      get_api_request(&full_config, secrets_path_str, &Provider::Local)?
+    // Use the first provider that has an API key
+    None => {
+      let req =
+        get_api_request(&full_config, secrets_path_str, &Default::default())
+          .or(get_api_request(
+            &full_config,
+            secrets_path_str,
+            &Model::Model(Provider::Groq, GROQ_MIXTRAL.to_string()),
+          ))
+          .or(get_api_request(
+            &full_config,
+            secrets_path_str,
+            &Model::Model(Provider::OpenAI, OPENAI_GPT_TURBO.to_string()),
+          ))?;
+      used_model = get_used_model(
+        &Model::Model(req.provider.clone(), req.model.clone()), //
+      );
+      req
     }
-    _ => get_api_request(&full_config, secrets_path_str, &Provider::Groq)
-      .unwrap_or(get_api_request(
-        &full_config,
-        secrets_path_str,
-        &Provider::OpenAI,
-      )?),
   };
 
   // This is checked here, so that the missing API key message comes first
@@ -158,21 +254,13 @@ async fn exec_tool(
     Err("No prompt was provided")?;
   }
 
-  if http_req.provider != Provider::Groq {
-    println!(
-      "ℹ️ Using {:#?} {}\n",
-      http_req.provider,
-      if http_req.model != "" {
-        format!("({})", http_req.model)
-      } else {
-        "".to_string()
-      }
-    )
-  }
-
   let req_body_obj = {
     let mut map = Map::new();
     map.insert("model".to_string(), Value::String(http_req.model));
+    map.insert(
+      "max_tokens".to_string(),
+      Value::Number(http_req.max_tokens.into()),
+    );
     map.insert(
       "messages".to_string(),
       Value::Array(vec![Value::Object(Map::from_iter([
@@ -184,39 +272,54 @@ async fn exec_tool(
   };
 
   let client = reqwest::Client::new();
-  let req = client
-    .post(http_req.url.clone())
-    .json(&req_body_obj)
-    .bearer_auth(http_req.api_key);
+  let req_base = client.post(http_req.url.clone()).json(&req_body_obj);
+  let req = match http_req.provider {
+    Provider::Anthropic => req_base
+      .header("anthropic-version", "2023-06-01")
+      .header("x-api-key", http_req.api_key),
+    _ => req_base.bearer_auth(http_req.api_key),
+  };
 
   let resp = req.send().await?;
 
   if !&resp.status().is_success() {
-    Err(resp.text().await?)?;
+    Err(format!(
+      "{used_model}{}\n",
+      resp.text().await? //
+    ))?;
   } else {
-    let res_json = &resp.json::<AiResponse>().await?;
-    let msg = &res_json.choices[0].message.content;
-
-    if termwidth() < 100 {
-      println!("{}", msg);
-    } else {
-      let msg_lines = textwrap::wrap(msg, 60);
-      for line in msg_lines {
-        println!("{}", line);
+    let mut msg = match http_req.provider {
+      Provider::Anthropic => {
+        let anth_response = resp.json::<AnthropicAiResponse>().await?;
+        anth_response.content[0].text.clone()
       }
+      _ => {
+        let ai_response = resp.json::<AiResponse>().await?;
+        ai_response.choices[0].message.content.clone()
+      }
+    };
+
+    if termwidth() > 100 {
+      msg = textwrap::wrap(&msg, 60).join("\n");
     }
+
+    let elapsed_time: String = start.elapsed().as_millis().to_string();
+
+    cprintln!(
+      "\n{used_model}\
+      <bold>⏱️ Duration: {elapsed_time} ms</bold>\n\
+      \n\
+      {msg}\n\
+      "
+    );
   }
   Ok(())
 }
 
-#[tokio::main]
-pub async fn submit_prompt(
-  optional_provider: &Option<Provider>,
-  user_input: &str,
-) {
+pub async fn submit_prompt(optional_model: &Option<Model>, user_input: &str) {
   // Necessary to wrap the execution function,
   // because a `main` function that returns a `Result` quotes any errors.
-  match exec_tool(optional_provider, user_input).await {
+  match exec_tool(optional_model, user_input).await {
     Ok(_) => (),
     Err(err) => {
       eprintln!("ERROR:\n{}", err);
@@ -232,7 +335,11 @@ mod tests {
   #[tokio::test]
   async fn test_submit_empty_prompt() {
     let prompt = "";
-    let result = exec_tool(&Some(Provider::OpenAI), &prompt).await;
+    let result = exec_tool(
+      &Some(Model::Model(Provider::OpenAI, OPENAI_GPT_TURBO.to_string())),
+      &prompt,
+    )
+    .await;
     assert!(result.is_err());
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,65 @@
-use cai::{submit_prompt, Provider};
+use cai::{
+  exec_tool, submit_prompt, Model, Provider, CLAUDE_HAIKU, CLAUDE_OPUS,
+  CLAUDE_SONNET, GROQ_MIXTRAL, OPENAI_GPT, OPENAI_GPT_TURBO,
+};
 use clap::{builder::styling, crate_version, Parser, Subcommand};
+use color_print::cformat;
+use futures::future::join_all;
 
 const CRATE_VERSION: &str = crate_version!();
 
 #[derive(Subcommand, Debug, PartialEq)]
 #[clap(args_conflicts_with_subcommands = true, arg_required_else_help(true))]
 enum Commands {
-  /// Use OpenAI's ChatGPT
-  #[clap(visible_alias = "g")]
+  /// Groq's Mixtral
+  #[clap(visible_alias = "mi")]
+  Mixtral {
+    /// The prompt to send to the AI model
+    prompt: Vec<String>,
+  },
+  /// OpenAI's GPT 4 Turbo
+  #[clap(visible_alias = "tu")]
+  GptTurbo {
+    /// The prompt to send to the AI model
+    prompt: Vec<String>,
+  },
+  /// OpenAI's GPT 4
+  #[clap(visible_alias = "gp")]
   Gpt {
     /// The prompt to send to the AI model
     prompt: Vec<String>,
   },
-  /// Use a local model hosted at http://localhost:8080 (e.g. Llamafile)
-  #[clap(visible_alias = "l")]
+  /// Anthropic's Claude Opus
+  #[clap(visible_alias = "op")]
+  ClaudeOpus {
+    /// The prompt to send to the AI model
+    prompt: Vec<String>,
+  },
+  /// Anthropic's Claude Sonnet
+  #[clap(visible_alias = "so")]
+  ClaudeSonnet {
+    /// The prompt to send to the AI model
+    prompt: Vec<String>,
+  },
+  /// 🏆 Default | Anthropic's Claude Haiku
+  #[clap(visible_alias = "ha")]
+  ClaudeHaiku {
+    /// The prompt to send to the AI model
+    prompt: Vec<String>,
+  },
+  /// Local model hosted at http://localhost:8080 (e.g. Llamafile)
+  #[clap(visible_alias = "lo")]
   Local {
     /// The prompt to send to the AI model
     prompt: Vec<String>,
   },
+  /// Send the prompt to every provider's default model simultaneously
+  /// (Anthropic's Claude Haiku, Groq's Mixtral, OpenAI's GPT 4 Turbo, Local)
+  All {
+    /// The prompt to send to the AI models simultaneously
+    prompt: Vec<String>,
+  },
+  // => https://stackoverflow.com/questions/51044467/how-can-i-perform-parallel-asynchronous-http-get-requests-with-reqwest
 }
 
 #[derive(Parser, Debug)]
@@ -42,21 +84,84 @@ struct Args {
   prompt: Vec<String>,
 }
 
-fn exec_with_args(args: Args) {
-  match &args.command {
-    Some(Commands::Gpt { prompt }) => {
-      submit_prompt(&Some(Provider::OpenAI), &prompt.join(" "))
-    }
-    Some(Commands::Local { prompt }) => {
-      submit_prompt(&Some(Provider::Local), &prompt.join(" "))
-    }
-    _ => submit_prompt(&None, &args.prompt.join(" ")),
+fn capitalize_str(str: &str) -> String {
+  let mut chars = str.chars();
+  match chars.next() {
+    None => String::new(),
+    Some(f) => f.to_uppercase().collect::<String>() + chars.as_str(),
   }
 }
 
-fn main() {
+async fn exec_with_args(args: Args) {
+  async fn call_submit_fn(
+    provider: Provider,
+    model_id: &str,
+    prompt: &Vec<String>,
+  ) {
+    submit_prompt(
+      &Some(Model::Model(provider, model_id.to_string())),
+      &prompt.join(" "),
+    )
+    .await
+  }
+
+  match args.command {
+    None => submit_prompt(&None, &args.prompt.join(" ")).await,
+    Some(cmd) => match cmd {
+      Commands::Mixtral { prompt } => {
+        call_submit_fn(Provider::Groq, GROQ_MIXTRAL, &prompt).await
+      }
+      Commands::GptTurbo { prompt } => {
+        call_submit_fn(Provider::OpenAI, OPENAI_GPT_TURBO, &prompt).await
+      }
+      Commands::Gpt { prompt } => {
+        call_submit_fn(Provider::OpenAI, OPENAI_GPT, &prompt).await
+      }
+      Commands::ClaudeOpus { prompt } => {
+        call_submit_fn(Provider::Anthropic, CLAUDE_OPUS, &prompt).await
+      }
+      Commands::ClaudeSonnet { prompt } => {
+        call_submit_fn(Provider::Anthropic, CLAUDE_SONNET, &prompt).await
+      }
+      Commands::ClaudeHaiku { prompt } => {
+        call_submit_fn(Provider::Anthropic, CLAUDE_HAIKU, &prompt).await
+      }
+      Commands::Local { prompt } => {
+        call_submit_fn(Provider::Local, "", &prompt).await //
+      }
+      Commands::All { prompt } => {
+        let models = vec![
+          Model::Model(Provider::Anthropic, CLAUDE_HAIKU.to_string()),
+          Model::Model(Provider::Groq, GROQ_MIXTRAL.to_string()),
+          Model::Model(Provider::OpenAI, OPENAI_GPT_TURBO.to_string()),
+          Model::Model(Provider::Local, "".to_string()),
+        ];
+
+        let mut handles = vec![];
+
+        for model in models.into_iter() {
+          let prompt_str = prompt.join(" ");
+          handles.push(tokio::spawn(async move {
+            match exec_tool(&Some(model), &prompt_str).await {
+              Ok(_) => {}
+              Err(err) => {
+                let err_fmt = capitalize_str(&err.to_string());
+                eprintln!("{}", cformat!("<red>ERROR:\n{err_fmt}\n</red>"))
+              }
+            }
+          }));
+        }
+
+        join_all(handles).await;
+      }
+    },
+  };
+}
+
+#[tokio::main]
+async fn main() {
   let args = Args::parse();
-  exec_with_args(args);
+  exec_with_args(args).await
 }
 
 #[cfg(test)]
